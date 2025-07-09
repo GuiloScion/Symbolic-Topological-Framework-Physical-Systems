0f928bba11be8d56dfcd49d5f28bd00982fba0bb
Noah Parsons <161479672+GuiloScion@users.noreply.github.com>
2025-07-09 10:37:10 -0600

    Create type_checker_code.py

---
+++ docs/dsl_spec/type_checker/type_checker_code.py
@@ -0,0 +1,11 @@
+Code:
+(Excerpt below; complete in source)
+
+class TypeChecker:
+    def __init__(self):
+        self.variables = {}  # name -> (type, Unit)
+        self.operators = {}  # name -> signature
+
+    def get_unit(self, unit_str): ...
+    def check_expression_type(self, expr: Expression): ...
+    def check(self, ast: List[ASTNode]):
